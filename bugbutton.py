424ac5f45fe04759a056b38c70b86529de187184
Enfostorm <2peter.vdt@gmail.com>
2021-04-23 16:37:11 +0200

    Refactoring the code to pull from external files
    Widgets can now be in separate .py and .kv files. They still have to be imported manually

---
+++ bugbutton.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+class BugButton(Widget):
+    functionLabel = ObjectProperty()
+    numberDisplay = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(BugButton, self).__init__(**kwargs)
+        self.counter = 0.0
+        self.firstrun = True
+
+    def setFunctionLabel(self, newLabel):
+        self.functionLabel.text = newLabel
+
+    def setValue(self, value, unit):        # Past de onderste tekstwaarde aan met een spatie tussen de twee.
+        self.numberDisplay.numberLabel.text = f'{value:.0f} {unit}' # Waarden weergeven met een f string (zet er hier een spatie tussen)
+
+class NumberDisplay(Widget):
+    numberLabel = ObjectProperty()
+
+# ========================================================================================
+class BugButtonApp(App):
+    def build(self):
+        bb = BugButton()
+        bb.setFunctionLabel('VSI')
+        bb.setValue(35000, 'ft')
+        return bb
+        
+
+
+def main():
+    BugButtonApp().run()
+
+
+if __name__ == '__main__':
+    main()
